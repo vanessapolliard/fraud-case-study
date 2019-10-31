ad5f22846ea6cce4fa04d1a8f9a8e4003baf2d97
Steven Rouk <stevenrouk@gmail.com>
2019-10-31 09:16:23 -0600

    Add comment.

--- src/database/populate.py
+++ src/database/populate.py
@@ -9,7 +9,7 @@ import requests
 FILE_DIRECTORY = os.path.split(os.path.realpath(__file__))[0]  # Directory this script is in
 SRC_DIRECTORY = os.path.split(FILE_DIRECTORY)[0]  # The 'src' directory
 ROOT_DIRECTORY = os.path.split(SRC_DIRECTORY)[0]  # The root directory for the project
-MODELS_DIRECTORY = os.path.join(ROOT_DIRECTORY, 'models')
+MODELS_DIRECTORY = os.path.join(ROOT_DIRECTORY, 'models')  # Directory for pickled models and model info
 DATA_DIRECTORY = os.path.join(ROOT_DIRECTORY, 'data')  # The data directory
 DATA_DIRECTORY_RAW = os.path.join(DATA_DIRECTORY, 'raw')  # The data/raw directory
 DATA_DIRECTORY_PROCESSED = os.path.join(DATA_DIRECTORY, 'processed')  # The data/processed directory
